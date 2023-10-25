15233980631c5a4301c07a41c73447e2a026321a
Mostefa Mourad <47932282+MostefaMourad@users.noreply.github.com>
2023-10-25 17:41:56 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import "./App.css";
+
+const recipes = [
+  {
+    author: "Jim",
+    name: "Chicken Curry",
+    description: "Delicious spicy chicken curry",
+  },
+  {
+    author: "Aravind",
+    name: "Hamburger",
+    description: "Juicy burger with toppings and a soft bun",
+  },
+];
+
+function App() {
+  return <div className="App">Let's add some content here</div>;
+}
+
+export default App;
